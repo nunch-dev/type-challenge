6268334c5425120fbed998dbf2a2a92685873882
nunch <ktojong@gmail.com>
2022-08-24 10:10:55 +0900

    :ambulance: Warming-Up 13_Hello World

---
+++ warming-up/hello-world.ts
@@ -0,0 +1,7 @@
+/* _____________ 여기에 코드 입력 _____________ */
+type HelloWorld = any; // expected to be a string
+
+/* _____________ 테스트 케이스 _____________ */
+import type { Equal, Expect, NotAny } from "@type-challenges/utils";
+
+type cases = [Expect<NotAny<HelloWorld>>, Expect<Equal<HelloWorld, string>>];
